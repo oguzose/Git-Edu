f390176cdd7b3286fec62d7e000d4785fba391c6
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2023-11-23 23:23:33 +0300

    Erdinç COMMIT

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("Deneme");
+        System.out.println("Erdinç tarafından buralar oluşturuldu");
     }
 }
